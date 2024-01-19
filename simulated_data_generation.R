defea378c72cd17e907d31a6d57525ea7a95428f
bbertesago <152092881+bbertesago@users.noreply.github.com>
2024-01-19 15:26:00 +0100

    Add files via upload

--- simulated_data_generation.R
+++ simulated_data_generation.R
@@ -7,12 +7,12 @@ seed = 22111996
 # Simulate a block graph
 set.seed(seed)
 p <- 40
-adj <- graph.sim(p = p, graph = "cluster", class = 3 , vis = T)
+adj <- graph.sim(p = p, graph = "cluster", class = 3 , vis = T, prob = 1)
 adj
 
 # Simulate the precision matrix given the graph
 set.seed(seed)
-K_true <- rgwish(n = 1, adj = adj)
+K_true <- rgwish(n = 1, adj = adj, D = 0.001*diag(p))
 
 # Simulate the beta coefficients given the precision matrix
 set.seed(seed)
@@ -21,6 +21,7 @@ x_mu <- seq(0, 1, length.out=p)
 mu_true = dgamma(x = x_mu, shape = 2, rate = 10) 
 plot(x_mu, mu_true, type='l')
 beta_true <- rmvnorm(n = n, mean =  mu_true , sigma=solve(K_true))
+plot(beta_true[1,], type='l')
 
 # Build the phi matrix
 r <- 200
@@ -30,7 +31,7 @@ BaseMat <- eval.basis(x, basis) # matrix r x p
 
 # Simulate the functional data 
 tau_eps = 100
-cov_y <- tau_eps * diag(1, r, r)
+cov_y <- 1/tau_eps * diag(1, r, r)
 y_hat_true = matrix(nrow = n, ncol = r)
 
 for (i in c(1:n)){
